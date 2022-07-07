f2a9292b12b9ceac1150d38bf6de446fbbb93acd
Gridy <reality023@naver.com>
2022-07-07 09:08:37 +0900

    FEAT : 모음 페이지 생성 코드 (링크 + 메모 타입) 병합 #44
    모음 페이지 링크 타입 생성 코드 추가
    모음 페이지 댓글 타입 임시 주석 처리
    postSlice -> moumSlice 로 변경
    LinkPieceCard 스타일 변경

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import user from "./modules/userSlice";
-import post from "./modules/postSlice";
+import moum from "./modules/moumSlice";
 import option from "./modules/optionSlice";
 import profile from "./modules/profileSlice"
 
 const store = configureStore({
-  reducer: { user, post, option, profile },
+  reducer: { user, moum, option, profile },
 });
 
 export default store;
---
+++ src/redux/modules/moumSlice.js
@@ -0,0 +1,131 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { instance } from "../../shared/axios"
+
+export const addPieceSimpleThunk = (data) => {
+  return async (dispatch) => {
+    try {
+      const moum = {
+        link: data.link,
+        status: "PUBLIC",
+        boardType: data.type,
+      }
+
+      const response = await instance.post("/board/simple", moum);
+      console.log(response);
+    } catch (err) {
+      console.log(err.response);
+    }
+  }
+}
+
+export const addPieceThunk = (data) => {
+  return async (dispatch) => {
+    if (data.share === "NONE") {
+      alert("공유 설정을 선택해주세요");
+      return;
+    }
+    if (data.type === "NONE") {
+      alert("조각의 타입을 선택해주세요");
+      return;
+    }
+
+    try {
+      const moum = {
+        title: data.subject,
+        link: data.link,
+        explanation: data.content,
+        imgPath: data.image,
+        status: data.share,
+        boardType: data.type,
+        category: data.category,
+      }
+      const response = await instance.post("/board/detail", moum);
+      // dispatch(addData({ id: response.data, ...data }));
+      console.log(response);
+    } catch (err) {
+      console.log(err.response);
+    }
+  }
+}
+
+export const getPieceThunk = () => {
+  return async (dispatch) => {
+    try {
+      const response = await instance.get("/board");
+      dispatch(setMoum(response.data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+// export const removeDataDB = (id) => {
+//   return async (dispatch) => {
+//     try {
+//       await instance.delete(`/board/${id}`);
+//       dispatch(removeData(id));
+//     } catch (err) {
+//       window.alert(err.response.data.message);
+//     }
+//   }
+// }
+
+// export const modifyDataDB = (id, data) => {
+//   return async (dispatch) => {
+//     try {
+//       await instance.put(`/board/${id}`, data);
+//       dispatch(modifyData({ id, data }));
+//     } catch (err) {
+//       console.log(err);
+//       window.alert(err.response.data.message);
+//     }
+//   }
+// }
+
+//Reducer
+
+const moumSlice = createSlice({
+  name: "post",
+  initialState: {
+    list: {
+      boardList: [],
+    }
+  },
+  reducers: {
+    setMoum: (state, action) => {
+      state.list = action.payload;
+    },
+    // addData: (state, action) => {
+    //   state.list.boardList.push(action.payload);
+    // },
+    // removeData: (state, action) => {
+    //   state.list.boardList = state.list.boardList.filter(
+    //     (post) => {
+    //       if (post.id === action.payload) {
+    //         return false;
+    //       } else {
+    //         return true;
+    //       }
+    //     }
+    //   )
+    // },
+    // modifyData: (state, action) => {
+    //   state.list.boardList = state.list.boardList.map(
+    //     (post) => {
+    //       if (post.id === action.payload.id) {
+    //         return {
+    //           ...post,
+    //           title: action.payload.data.title,
+    //           content: action.payload.data.content
+    //         }
+    //       } else {
+    //         return post;
+    //       }
+    //     }
+    //   );
+    // }
+  }
+});
+
+export const { setMoum } = moumSlice.actions;
+export default moumSlice.reducer;
