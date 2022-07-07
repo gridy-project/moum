1198a51344871cb1785571457c2168f31e1de11c
Gridy <reality023@naver.com>
2022-07-07 23:01:30 +0900

    FEAT : 커스텀 훅 : 로그인 상태 확인 훅 / handleChange 훅 추가

---
+++ src/hooks/useHandleChange.jsx
@@ -0,0 +1,9 @@
+import { useCallback, useState } from "react";
+
+function useHandleChange (init) {
+  const [input, setInput] = useState(init);
+  const handleChange = useCallback((name) => (e) => setInput((current) => ({...current, [name]: e.target.value})), []);
+  return [input, setInput, handleChange];
+}
+
+export default useHandleChange;
