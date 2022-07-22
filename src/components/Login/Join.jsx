06cb82ed59d2e54046503c0345301ed20db1ac81
jrl103 <jrl103@naver.com>
2022-07-23 02:51:12 +0900

    회원가입 페이지 기능 구현 #111

--- src/components/Login/Join.jsx
+++ src/components/Login/Join.jsx
@@ -1,30 +1,178 @@
 // React
-import React from 'react';
+import React, { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+// React Query
+import { useMutation } from "react-query";
+import useCustomMutate from "hooks/useCustomMutate";
+// axios
+import { instance }  from "shared/axios"
+// import { executeCheckEmailAxios, executeCheckNickAxios, executeSignUpAxios } from "utils/api/auth";
 // css
-import styled from "styled-components";
+import styled, { css }  from "styled-components";
 
 const Join = () => {
+  // const navigate = useNavigate();
+
+  const [active, setActive] = useState(false);
+  
+  const ref = {
+    username: useRef(),
+    nickname: useRef(),
+    password: useRef(),
+    passwordConfirm: useRef(),
+    email: useRef(),
+    certification: useRef()
+  }
+
+  // 회원가입 이메일인증 메일전송
+  const CheckEmailRegister = () => {
+     const data = {
+      email : ref.email.current.value
+    }
+    sendEmailCheckJoinCode(data);
+  }
+
+  const { mutate: sendEmailCheckJoinCode } = useMutation(
+    async (data) => {
+      const response = await instance.post("/email", data);
+      return response.data;
+    },
+    {
+      onSuccess: (data) => {
+        console.log(data);
+      },
+			onError: (err) => {
+				console.log(err)
+        if (err.statusCode === 501) {
+          window.alert(err.message);
+        } else if (err.statusCode === 500) {
+          window.alert(err.message);
+        }
+			}
+    }
+  )
+
+ // 인증번호 확인
+  const clickEmailCheck = () => {
+    const data = {
+      email : ref.email.current.value,
+      certification : ref.certification.current.value
+    }
+    EmailCheckJoin(data);
+  }
+
+  const { mutate: EmailCheckJoin} = useMutation(
+    async (data) => {
+      const response = await instance.post("/email/check", data);
+      return response.data;
+    },
+    {
+      onSuccess: (data) => {
+        console.log(data);       
+      },
+			onError: (err) => {
+				console.log(err)
+			}
+    }
+  ) 
+
+  // input 에 값이 있을 경우 확인 버튼 활성화
+  const checkInputCount = () => {
+    if (ref.certification.current.value.length > 0){
+			setActive(true);
+		} else {
+      setActive(false);
+    }
+  }
+
+  // 아이디 중복 확인
+  const clickIdCheck = () => {
+    IdCheckJoin();
+  }
+
+  const { mutate: IdCheckJoin} = useMutation(
+    async () => {
+      const response = await instance.get("/user/emailDupCheck/{username}");
+      return response.data;
+    },
+    {
+      onSuccess: (data) => {
+        console.log(data);       
+      },
+			onError: (err) => {
+				console.log(err)
+			}
+    }
+  ) 
+
+  // 비밀번호 일치/불일치 확인
+  const clickPwdCheck = () => {
+    const password = ref.password.current.value;
+    const passwordConfirm = ref.passwordConfirm.current.value;
+
+     if (password !== passwordConfirm) {
+      alert("비밀번호가 일치하지 않습니다.");
+      return;
+    }
+  }
+
+  // 회원가입
+   const clickSignUp = () => {
+    const data = {
+      username: ref.username.current.value,
+      nickname: ref.username.current.value,
+      password: ref.password.current.value,
+      email : ref.email.current.value
+    }
+    signUp(data);
+  }
+
+  const { mutate: signUp} = useMutation(
+    async (data) => {
+      const response = await instance.post("/user/signup", data);
+      return response.data;
+    },
+    {
+      onSuccess: (data) => {
+        console.log(data);       
+      },
+			onError: (err) => {
+				console.log(err)
+			}
+    }
+  ) 
+
   return (
     <JoinContainer>
       <JoinTitle>회원가입</JoinTitle>
-      <JoinCheckBox>
-        <p>이메일로 본인 확인</p>
-        <JoinEmailBox>
-          <input type="text" placeholder='이메일' />
-          <button>인증요청</button>
-        </JoinEmailBox>
-        <JoinCodeBox>
-          <input type="text" placeholder='인증코드를 입력해주세요.' />
-          <button>확인</button>
-        </JoinCodeBox>
-      </JoinCheckBox>
-      <CreateBox>
-        <p>계정 만들기</p>
-        <input type="text" placeholder='아이디' />
-        <input type="text" placeholder='비밀번호 (숫자, 영문자 포함 4자 이상)' />
-        <input type="text" placeholder='비밀번호 확인' />
-      </CreateBox>
-      <JoinBtn>회원가입</JoinBtn>
+        <JoinCheckBox>
+          <p>이메일로 본인 확인</p>
+          <JoinEmailBox>
+            <input type="text" ref={ref.email} placeholder='이메일' autoComplete="email"/>
+            <button onClick={CheckEmailRegister}>인증요청</button>
+          </JoinEmailBox>
+          <JoinCodeBox isActive={active}>
+            <input 
+            type="text"         
+            onChange={checkInputCount}
+            placeholder='인증코드를 입력해주세요.'
+            ref={ref.certification}  />
+            <button 
+            onClick={clickEmailCheck}>
+            확인</button>
+          </JoinCodeBox>
+        </JoinCheckBox>
+        <CreateBox>
+          <p>계정 만들기</p>
+          <input type="text" ref={ref.username} placeholder='아이디' />
+          <input type="password" ref={ref.password} autoComplete="password" placeholder='비밀번호 (숫자, 영문자 포함 4자 이상)' />
+          <input type="password" ref={ref.passwordConfirm} autoComplete="new-password" placeholder='비밀번호 확인' />
+        </CreateBox>
+        <JoinBtn onClick={() => {
+          clickIdCheck();   
+          clickPwdCheck();
+          clickSignUp();
+        }}>회원가입</JoinBtn>
     </JoinContainer>
   )
  }
@@ -89,6 +237,17 @@ const JoinCodeBox = styled.div`
     color: #8B8B8B;
     margin-left:8px;
     cursor: pointer;
+    ${(props) =>
+      props.isActive ? 
+      css`
+        background-color: #9152ff;
+				color: #ffffff;
+      `:
+      css`
+        background-color: #f6f5fb;
+        color: #9152ff;
+      `
+    }
   }
 `;
 const CreateBox = styled.div`
--- src/components/Login/ReissuePwd.jsx
+++ src/components/Login/ReissuePwd.jsx
@@ -3,9 +3,9 @@ import React, { useState, useRef } from 'react';
 // React Query
 import { useMutation } from "react-query";
 // axios
-import { instance } from "shared/axios"
+import { instance }  from "shared/axios"
 // css
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
 
 const ReissuePwd = () => {
   const idCheckRef = useRef();
@@ -16,6 +16,7 @@ const ReissuePwd = () => {
 
   // 비밀번호 재설정 인증 메일 발송
   const ClickResetPwdCode = () => {
+   
      const data = {
       username : idCheckRef.current.value,
       email : emailCheckRef.current.value
@@ -31,7 +32,6 @@ const ReissuePwd = () => {
     {
       onSuccess: (data) => {
         console.log(data);
-        window.alert(data.message);
       },
 			onError: (err) => {
 				console.log(err)
@@ -43,6 +43,30 @@ const ReissuePwd = () => {
 			}
     }
   )
+
+ // 인증번호 확인
+  const clickEmailCheck = () => {
+     const data = {
+      email : emailCheckRef.current.value,
+      certification : CodeCheckRef.current.value
+    }
+    EmailCheck(data);
+  }
+
+  const { mutate: EmailCheck} = useMutation(
+    async (data) => {
+      const response = await instance.post("/email/check", data);
+      return response.data;
+    },
+    {
+      onSuccess: (data) => {
+        console.log(data);       
+      },
+			onError: (err) => {
+				console.log(err)
+			}
+    }
+  ) 
 
   // 임시 비밀번호 발급
     const clickSendNewPwd = () => {
@@ -61,11 +85,9 @@ const ReissuePwd = () => {
     {
       onSuccess: (data) => {
         console.log(data);       
-        window.alert(data.message);
       },
 			onError: (err) => {
 				console.log(err)
-        window.alert(err.message);
 			}
     }
   )
@@ -99,11 +121,11 @@ const ReissuePwd = () => {
           onChange={checkInputCount}
           placeholder='인증코드를 입력해주세요.'/>
           <button         
-          onClick={clickSendNewPwd}
+          onClick={clickEmailCheck}
           >확인</button>  
         </PwdCodeBox>
       </PwdCheckEmail>
-      <PwdBtn>새 비밀번호 받기</PwdBtn>
+      <PwdBtn onClick={clickSendNewPwd}>새 비밀번호 받기</PwdBtn>
     </PwdContainer>
   )
  }
@@ -196,7 +218,6 @@ const PwdCodeBox = styled.div`
         color: #9152ff;
       `
     }
- 
   }
 `
 const PwdBtn = styled.button`
